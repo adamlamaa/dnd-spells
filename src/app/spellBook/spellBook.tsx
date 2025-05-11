ad0b76d8b9806939caf25a9c244b6799cb0dca1f
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-05-11 19:52:54 +0200

    Sort spells by level

--- src/app/spellBook/spellBook.tsx
+++ src/app/spellBook/spellBook.tsx
@@ -11,6 +11,11 @@ export const SpellBook = ({}: SpellBookProps) => {
     <div className={"grid grid-cols-3 gap-4"}>
       {spells
         .filter((spell: Spell) => spell.classes.includes("paladin"))
+        .sort((a: Spell, b: Spell) => {
+          const levelA = a.level === "cantrip" ? 0 : parseInt(a.level, 10)
+          const levelB = b.level === "cantrip" ? 0 : parseInt(b.level, 10)
+          return levelA - levelB
+        })
         .map((spell: Spell, index) => (
           <Card spell={spell} key={index} />
         ))}
