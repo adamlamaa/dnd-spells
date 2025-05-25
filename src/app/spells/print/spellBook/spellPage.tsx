ab15aa536083fe5ecd0357147fed4069b6bd302a
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-05-25 19:12:01 +0200

    Filter printed spells based on user selection

--- src/app/spells/print/spellBook/spellPage.tsx
+++ src/app/spells/print/spellBook/spellPage.tsx
@@ -1,29 +1,53 @@
 import { SpellCard } from "./card/spellCard"
 import spells from "../../../../spells.json"
-import type { Spell } from "@/types/spell"
+import type { Spell, SpellLevel, SpellSubClass } from "@/types/spell"
+import { useSpellStore } from "@/app/spells/spells.store"
+import { useShallow } from "zustand/react/shallow"
 
 interface SpellPageProps {
   id?: string
 }
 
 export const SpellPage = ({}: SpellPageProps) => {
+  const { filters } = useSpellStore(
+    useShallow((state) => ({
+      filters: state.filters,
+    })),
+  )
+
+  const spellLevelsInScope = (
+    filters.level.size === 0
+      ? [...spells]
+      : spells.filter((spell) => filters.level.has(spell.level as SpellLevel))
+  ) as Spell[]
+
+  console.log("SpellPage filters", spellLevelsInScope)
+
+  const spellsInScope =
+    filters.class.size === 0 &&
+    filters.subclasses.size === 0 &&
+    filters.spells.size === 0
+      ? spellLevelsInScope
+      : spellLevelsInScope.filter(
+          (spell) =>
+            spell.classes.some((item) => filters.class.has(item)) ||
+            spell.tags.some((item) =>
+              filters.subclasses.has(item as SpellSubClass),
+            ) ||
+            filters.spells.has(spell.name),
+        )
+
+  const spellsSorted = spellsInScope.sort((a, b) => {
+    const levelA = a.level === "cantrip" ? 0 : parseInt(a.level, 10)
+    const levelB = b.level === "cantrip" ? 0 : parseInt(b.level, 10)
+    return levelA - levelB
+  })
+
   return (
     <div className={"grid grid-cols-3 gap-1"}>
-      {(spells as Spell[])
-        .filter(
-          (spell) =>
-            spell.classes.includes("paladin") ||
-            spell.name === "Confusion" ||
-            spell.tags.includes("oath_of_vengeance"),
-        )
-        .sort((a, b) => {
-          const levelA = a.level === "cantrip" ? 0 : parseInt(a.level, 10)
-          const levelB = b.level === "cantrip" ? 0 : parseInt(b.level, 10)
-          return levelA - levelB
-        })
-        .map((spell, index) => (
-          <SpellCard spell={spell} key={index} />
-        ))}
+      {spellsSorted.map((spell, index) => (
+        <SpellCard spell={spell} key={index} />
+      ))}
     </div>
   )
 }
