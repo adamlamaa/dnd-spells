506e3de14ab5b73eaba1814817aedff7e227e2d9
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-06-01 14:45:22 +0200

    Show print count

--- src/app/spells/print/print.tsx
+++ src/app/spells/print/print.tsx
@@ -1,6 +1,10 @@
 import { SpellPage } from "@/app/spells/print/spellPage/spellPage"
 import { useRef } from "react"
 import { useReactToPrint } from "react-to-print"
+import { useSpellStore } from "@/app/spells/spells.store"
+import { useShallow } from "zustand/react/shallow"
+import spells from "@/spells.json"
+import type { Spell, SpellLevel, SpellSubClass } from "@/types/spell"
 
 interface PrintProps {
   id?: string
@@ -13,9 +17,43 @@ export const Print = ({}: PrintProps) => {
     documentTitle: "DND Spells",
   })
 
+  const { filters } = useSpellStore(
+    useShallow((state) => ({
+      filters: state.filters,
+    })),
+  )
+
+  const spellLevelsInScope = (
+    filters.level.size === 0
+      ? [...spells]
+      : spells.filter((spell) => filters.level.has(spell.level as SpellLevel))
+  ) as Spell[]
+
+  const spellsInScope =
+    filters.class.size === 0 &&
+    filters.subclasses.size === 0 &&
+    filters.spells.size === 0
+      ? spellLevelsInScope
+      : spellLevelsInScope.filter(
+          (spell) =>
+            spell.classes.some((item) => filters.class.has(item)) ||
+            spell.tags.some((item) =>
+              filters.subclasses.has(item as SpellSubClass),
+            ) ||
+            filters.spells.has(spell.name),
+        )
+
+  const spellsSorted = spellsInScope.sort((a, b) => {
+    const levelA = a.level === "cantrip" ? 0 : parseInt(a.level, 10)
+    const levelB = b.level === "cantrip" ? 0 : parseInt(b.level, 10)
+    return levelA - levelB
+  })
+
   return (
     <>
-      <div>With current selection: X / 400</div>
+      <div>
+        With current selection: {spellsSorted.length} / {spells.length}
+      </div>
       <button
         onClick={reactToPrintFn}
         className={
@@ -28,7 +66,7 @@ export const Print = ({}: PrintProps) => {
         ref={contentRef}
         className={"print-preview hidden w-full print:block"}
       >
-        <SpellPage />
+        <SpellPage spells={spellsSorted} />
       </div>
     </>
   )
--- src/app/spells/print/spellPage/spellPage.tsx
+++ src/app/spells/print/spellPage/spellPage.tsx
@@ -1,49 +1,14 @@
 import { SpellCard } from "./card/spellCard"
-import spells from "../../../../spells.json"
-import type { Spell, SpellLevel, SpellSubClass } from "@/types/spell"
-import { useSpellStore } from "@/app/spells/spells.store"
-import { useShallow } from "zustand/react/shallow"
+import type { Spell } from "@/types/spell"
 
 interface SpellPageProps {
-  id?: string
+  spells: Spell[]
 }
 
-export const SpellPage = ({}: SpellPageProps) => {
-  const { filters } = useSpellStore(
-    useShallow((state) => ({
-      filters: state.filters,
-    })),
-  )
-
-  const spellLevelsInScope = (
-    filters.level.size === 0
-      ? [...spells]
-      : spells.filter((spell) => filters.level.has(spell.level as SpellLevel))
-  ) as Spell[]
-
-  const spellsInScope =
-    filters.class.size === 0 &&
-    filters.subclasses.size === 0 &&
-    filters.spells.size === 0
-      ? spellLevelsInScope
-      : spellLevelsInScope.filter(
-          (spell) =>
-            spell.classes.some((item) => filters.class.has(item)) ||
-            spell.tags.some((item) =>
-              filters.subclasses.has(item as SpellSubClass),
-            ) ||
-            filters.spells.has(spell.name),
-        )
-
-  const spellsSorted = spellsInScope.sort((a, b) => {
-    const levelA = a.level === "cantrip" ? 0 : parseInt(a.level, 10)
-    const levelB = b.level === "cantrip" ? 0 : parseInt(b.level, 10)
-    return levelA - levelB
-  })
-
+export const SpellPage = ({ spells }: SpellPageProps) => {
   return (
     <div className={"grid grid-cols-3 gap-1"}>
-      {spellsSorted.map((spell, index) => (
+      {spells.map((spell, index) => (
         <SpellCard spell={spell} key={index} />
       ))}
     </div>
