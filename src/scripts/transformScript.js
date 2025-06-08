613d8cf75c9073f285c34c7725d904adb8e79ad4
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-06-08 16:44:38 +0200

    TransformScript to quickly update spells

---
+++ src/scripts/transformScript.js
@@ -0,0 +1,45 @@
+import * as fs from "fs"
+
+// Read the spells.json file
+const spellsFilePath = "src/spellsold.json"
+const newSpellsFilePath = "src/spellsnew.json"
+
+fs.readFile(spellsFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading spells.json:", err)
+    return
+  }
+
+  try {
+    // Parse the JSON data
+    const spells = JSON.parse(data)
+
+    // Transform the description field
+    const updatedSpells = spells.map((spell) => {
+      const { description, description_continued, ...rest } = spell
+      const newDescription = [description]
+      if (
+        description_continued !== undefined &&
+        description_continued !== null
+      ) {
+        newDescription.push(description_continued)
+      }
+      return { ...rest, description: newDescription }
+    })
+
+    // Write the updated data to spellnew.json
+    fs.writeFile(
+      newSpellsFilePath,
+      JSON.stringify(updatedSpells, null, 2),
+      (err) => {
+        if (err) {
+          console.error("Error writing spellnew.json:", err)
+        } else {
+          console.log("Updated spells saved to spellnew.json")
+        }
+      },
+    )
+  } catch (parseError) {
+    console.error("Error parsing JSON:", parseError)
+  }
+})
