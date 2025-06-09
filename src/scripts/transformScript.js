5a2b9cc104c619cf0a399fbbfbfd81c53a6f0e08
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-06-09 20:52:30 +0200

    Sort spells alphabetically

--- src/scripts/transformScript.js
+++ src/scripts/transformScript.js
@@ -1,7 +1,7 @@
 import * as fs from "fs"
 
 // Read the spells.json file
-const spellsFilePath = "src/spellsold.json"
+const spellsFilePath = "src/spells.json"
 const newSpellsFilePath = "src/spellsnew.json"
 
 fs.readFile(spellsFilePath, "utf8", (err, data) => {
@@ -15,16 +15,25 @@ fs.readFile(spellsFilePath, "utf8", (err, data) => {
     const spells = JSON.parse(data)
 
     // Transform the description field
-    const updatedSpells = spells.map((spell) => {
-      const { description, description_continued, ...rest } = spell
-      const newDescription = [description]
-      if (
-        description_continued !== undefined &&
-        description_continued !== null
-      ) {
-        newDescription.push(description_continued)
+    // const updatedSpells = spells.map((spell) => {
+    //   const { description, description_continued, ...rest } = spell
+    //   const newDescription = [description]
+    //   if (
+    //     description_continued !== undefined &&
+    //     description_continued !== null
+    //   ) {
+    //     newDescription.push(description_continued)
+    //   }
+    //   return { ...rest, description: newDescription }
+    // })
+
+    // sort by name
+    const updatedSpells = spells.sort((a, b) => {
+      {
+        if (a.name < b.name) return -1
+        if (a.name > b.name) return 1
+        return 0
       }
-      return { ...rest, description: newDescription }
     })
 
     // Write the updated data to spellnew.json
