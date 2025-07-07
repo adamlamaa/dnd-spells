c4d54830a000b56c21062143ca72b0157a35b382
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-07-07 20:24:45 +0200

    Adjust styling for header, mobile support

--- src/app/introduction/introductionSection.tsx
+++ src/app/introduction/introductionSection.tsx
@@ -4,8 +4,14 @@ import Image from "next/image"
 
 export const IntroductionSection = () => {
   return (
-    <GlossyBox className={"grid grid-cols-5 gap-20 p-8"}>
-      <div className={"col-span-2 items-center justify-center py-4"}>
+    <GlossyBox
+      className={
+        "grid grid-cols-3 place-items-center gap-10 p-8 md:grid-cols-5 md:gap-20"
+      }
+    >
+      <div
+        className={"col-span-3 items-center justify-center py-4 md:col-span-2"}
+      >
         This website is designed to help players of DND 5e Legacy Edition easily
         print out their spells, offering a convenient way to bring their spell
         lists to the table for a more tactile, analogue gameplay experience. By
@@ -13,45 +19,46 @@ export const IntroductionSection = () => {
         with quick reference and hands-on interaction, making it easier to
         manage your character’s magic during your adventures.
       </div>
-      <div className={"col-span-3 flex items-center justify-center gap-1"}>
-        <TiltedCard startRotate={"rotate-6"}>
+      {/*<div className={"col-span-3 flex items-center justify-center gap-1"}>*/}
+      <div className={"col-span-3 grid grid-cols-2 gap-1 md:grid-cols-5"}>
+        <TiltedCard startRotate={"rotate-6"} backgroundColor={"bg-violet-500"}>
           <Image
             src={"/spells/Spell_Fireball.png"}
             alt={"Fireball spell card example"}
-            width={250}
-            height={250}
+            width={135}
+            height={170}
           />
         </TiltedCard>
-        <TiltedCard startRotate={"-rotate-4"}>
+        <TiltedCard startRotate={"-rotate-4"} backgroundColor={"bg-violet-700"}>
           <Image
             src={"/spells/Spell_DetectMagic.png"}
             alt={"Detect magic spell card example"}
-            width={250}
-            height={250}
+            width={135}
+            height={170}
           />
         </TiltedCard>
-        <TiltedCard startRotate={"rotate-8"}>
+        <TiltedCard startRotate={"rotate-8"} backgroundColor={"bg-violet-600"}>
           <Image
             src={"/spells/Spell_MinorIllusion.png"}
             alt={"Minor Illusion spell card example"}
-            width={250}
-            height={250}
+            width={135}
+            height={170}
           />
         </TiltedCard>
-        <TiltedCard startRotate={"-rotate-6"}>
+        <TiltedCard startRotate={"-rotate-6"} backgroundColor={"bg-violet-500"}>
           <Image
             src={"/spells/Spell_MinorIllusion_2.png"}
             alt={"Minor Illusion continued spell card example"}
-            width={250}
-            height={250}
+            width={135}
+            height={170}
           />
         </TiltedCard>
-        <TiltedCard startRotate={"rotate-12"}>
+        <TiltedCard startRotate={"rotate-12"} backgroundColor={"bg-violet-700"}>
           <Image
             src={"/spells/Spell_Shield.png"}
             alt={"Shield spell card example"}
-            width={250}
-            height={250}
+            width={135}
+            height={170}
           />
         </TiltedCard>
       </div>
--- src/components/ui/tiltedCard.tsx
+++ src/components/ui/tiltedCard.tsx
@@ -4,19 +4,22 @@ import type { ReactNode } from "react"
 
 interface TiltedCardProps {
   startRotate?: ClassValue
+  backgroundColor?: ClassValue
   children?: ReactNode
 }
 
 export const TiltedCard = ({
   startRotate = "rotate-6",
+  backgroundColor = "bg-violet-600",
   children,
 }: TiltedCardProps) => {
   return (
     <div
       className={cn(
-        "-rotate-6 cursor-pointer rounded-2xl bg-violet-500 transition-transform duration-300",
+        "-rotate-6 cursor-pointer rounded-2xl transition-transform duration-300",
         "hover:z-20 hover:scale-130 hover:rotate-0 hover:shadow-lg",
         startRotate,
+        backgroundColor,
       )}
     >
       {children}
