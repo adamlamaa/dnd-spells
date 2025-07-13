488d1e57cc3a6cbea0395515d6e8fb888b70fa03
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-07-13 16:47:13 +0200

    Omit accessories for now

--- src/app/introduction/introductionSection.tsx
+++ src/app/introduction/introductionSection.tsx
@@ -1,7 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { About } from "@/app/introduction/about/about"
 import { Details } from "@/app/introduction/details/details"
-import { Accessories } from "@/app/introduction/accessories/accessories"
 import { Support } from "@/app/introduction/support/support"
 
 const ABOUT_TAB = "about"
@@ -19,7 +18,7 @@ export const IntroductionSection = () => {
         <TabsList>
           <TabsTrigger value={ABOUT_TAB}>About</TabsTrigger>
           <TabsTrigger value={DETAILS_TAB}>Details</TabsTrigger>
-          <TabsTrigger value={ACCESSORIES_TAB}>Accessories</TabsTrigger>
+          {/*<TabsTrigger value={ACCESSORIES_TAB}>Accessories</TabsTrigger>*/}
           <TabsTrigger value={SUPPORT_TAB}>Support</TabsTrigger>
         </TabsList>
         <TabsContent value={ABOUT_TAB} className={"w-full"}>
@@ -28,9 +27,9 @@ export const IntroductionSection = () => {
         <TabsContent value={DETAILS_TAB} className={"w-full"}>
           <Details />
         </TabsContent>
-        <TabsContent value={ACCESSORIES_TAB} className={"w-full"}>
-          <Accessories />
-        </TabsContent>
+        {/*<TabsContent value={ACCESSORIES_TAB} className={"w-full"}>*/}
+        {/*  <Accessories />*/}
+        {/*</TabsContent>*/}
         <TabsContent value={SUPPORT_TAB} className={"w-full"}>
           <Support />
         </TabsContent>
