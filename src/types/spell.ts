8f871ea236cdb3662a18a63b25893f727bce5421
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-07-26 10:33:33 +0200

    Update spell subclasses

--- src/types/spell.ts
+++ src/types/spell.ts
@@ -28,8 +28,27 @@ export const SpellClasses = {
 export type SpellClass = keyof typeof SpellClasses
 
 export const SpellSubClasses = {
-  cleric_trickery: "Trickery",
-  paladin_oath_of_vengeance: "Oath of Vengeance",
+  cleric_knowledge: "Cleric: Knowledge Domain",
+  cleric_life: "Cleric: Life Domain",
+  cleric_light: "Cleric: Light Domain",
+  cleric_nature: "Cleric: Nature Domain",
+  cleric_tempest: "Cleric: Tempest Domain",
+  cleric_trickery: "Cleric: Trickery Domain",
+  cleric_war: "Cleric: War Domain",
+  druid_arctic: "Druid: Arctic Circle",
+  druid_coast: "Druid: Coast Circle",
+  druid_desert: "Druid: Desert Circle",
+  druid_forest: "Druid: Forest Circle",
+  druid_grassland: "Druid: Grassland Circle",
+  druid_mountain: "Druid: Mountain Circle",
+  druid_swamp: "Druid: Swamp Circle",
+  druid_underdark: "Druid: Underdark Circle",
+  paladin_ancients: "Paladin: Oath of the Ancients",
+  paladin_devotion: "Paladin: Oath of Devotion",
+  paladin_vengeance: "Paladin: Oath of Vengeance",
+  warlock_archfey: "Warlock: The Archfey",
+  warlock_fiend: "Warlock: The Fiend",
+  warlock_great_old_one: "Warlock: The Great Old One",
 }
 
 export type SpellSubClass = keyof typeof SpellSubClasses
