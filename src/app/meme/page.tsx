c803601ad07ac2fa8f142a43cebd6be3168faed0
Adam Kvist Lamaa <kvistlamaa@outlook.com>
2025-08-14 20:52:54 +0200

    Fix meme random generator

--- src/app/meme/page.tsx
+++ src/app/meme/page.tsx
@@ -1,25 +1,31 @@
-import { Suspense } from "react"
+"use client"
+
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
 export default function HomePage() {
+  const [memeIndex, setMemeIndex] = useState(0)
+
+  useEffect(() => {
+    setMemeIndex(Math.floor(Math.random() * 9))
+  }, [])
+
   return (
-    <Suspense>
-      <div className="flex flex-col gap-10 p-16">
-        <header className={"flex flex-col items-center"}>
-          <Link href={"/"}>
-            <h1 className="text-5xl font-extrabold">DND Spells</h1>
-          </Link>
-        </header>
-        <main className={"flex w-full flex-col items-center gap-10"}>
-          <Image
-            src={`/memes/${Math.floor(Math.random() * 9)}.png`}
-            alt={"Meme"}
-            width={500}
-            height={500}
-          />
-        </main>
-      </div>
-    </Suspense>
+    <div className="flex flex-col gap-10 p-16">
+      <header className={"flex flex-col items-center"}>
+        <Link href={"/"}>
+          <h1 className="text-5xl font-extrabold">DND Spells</h1>
+        </Link>
+      </header>
+      <main className={"flex w-full flex-col items-center gap-10"}>
+        <Image
+          src={`/memes/${memeIndex}.png`}
+          alt={"Meme"}
+          width={500}
+          height={500}
+        />
+      </main>
+    </div>
   )
 }
